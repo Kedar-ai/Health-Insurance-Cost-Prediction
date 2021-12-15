c35ec071b4f69c10f2451209980aa0bf40153f3c
Kedar Bhingarde <47631008+Kedar-ai@users.noreply.github.com>
2021-12-15 16:44:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib
 import math
 from PIL import Image
-page_config = {"page_title":'Medical Insurance',"page_icon":'U+1F1F0'}
+page_config = {"page_title":'Medical_Insurance_KB',"page_icon":'U+1F1F0'}
 st.set_page_config(**page_config)
 
 def predict_insurance_cost(age,sex,bmi,children,smoker,region):
