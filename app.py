7d28130e0f3801bd847867288d47c74d9ed2d72d
Kedar Bhingarde <47631008+Kedar-ai@users.noreply.github.com>
2021-12-15 16:32:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import joblib
 import math
 from PIL import Image
 
-st.set_page_config(page_title='Medical Insurance',page_icon='��')
+st.set_page_config(page_title='Medical Insurance',page_icon='🇰🇧')
 
 def predict_insurance_cost(age,sex,bmi,children,smoker,region):
     prediction=model.predict([[age,sex,bmi,children,smoker,region]])
