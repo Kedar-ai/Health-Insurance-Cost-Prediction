8e89061e4bc5a94bbed5779e01e26ad0d24c943b
Kedar Bhingarde <47631008+Kedar-ai@users.noreply.github.com>
2021-12-17 08:34:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ choice = st.sidebar.selectbox("", menu)
 
 if choice == "Application":
     html_temp = """
-        <div style="background-color:green;padding:20px">
-        <h1 style="color:white";text-align:center> Health Insurance Cost Prediction ⚕️</h1>
+        <div style="background-color:green;padding:15px">
+        <h1 style="color:white";text-align:center> Health Insurance Cost Prediction</h1>
         </div>
         """
     st.markdown(html_temp, unsafe_allow_html=True)
